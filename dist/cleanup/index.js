829da9aa69811adb0627c86d566c856f30f04a08
Aparna Jyothi <aparnajyothi-y@github.com>
2025-05-16 15:01:57 +0530

    regex update

--- dist/cleanup/index.js
+++ dist/cleanup/index.js
@@ -94229,7 +94229,7 @@ function getVersionFromFileContent(content, distributionName, versionFile) {
     const versionFileName = getFileName(versionFile);
     if (versionFileName == '.tool-versions') {
         javaVersionRegExp =
-            /^java\s+(?:\S*-)?(?<version>\d+(?:\.\d+)*)(?:(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|\d+[a-z]*))*)(?:\+\S+)?$/im;
+            /^java\s+(?:\S*-)?(?<version>\d+(?:\.\d+)*)(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|u\d+|\d+[a-z]*))*(?:\+[\w.\-]+)?/im;
     }
     else {
         javaVersionRegExp = /(?<version>(?<=(^|\s|-))(\d+\S*))(\s|$)/;
--- src/util.ts
+++ src/util.ts
@@ -133,7 +133,7 @@ export function getVersionFromFileContent(
   const versionFileName = getFileName(versionFile);
   if (versionFileName == '.tool-versions') {
     javaVersionRegExp =
-      /^java\s+(?:\S*-)?(?<version>\d+(?:\.\d+)*)(?:(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|\d+[a-z]*))*)(?:\+\S+)?$/im;
+      /^java\s+(?:\S*-)?(?<version>\d+(?:\.\d+)*)(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|u\d+|\d+[a-z]*))*(?:\+[\w.\-]+)?/im;
   } else {
     javaVersionRegExp = /(?<version>(?<=(^|\s|-))(\d+\S*))(\s|$)/;
   }
