be267e357a4c29a1445031687f6c919e0ec04070
Aparna Jyothi <aparnajyothi-y@github.com>
2025-05-19 17:35:04 +0530

    regex update

--- dist/cleanup/index.js
+++ dist/cleanup/index.js
@@ -94229,7 +94229,7 @@ function getVersionFromFileContent(content, distributionName, versionFile) {
     const versionFileName = getFileName(versionFile);
     if (versionFileName == '.tool-versions') {
         javaVersionRegExp =
-            /^java\s+(?:\S*-)?(?<version>\d+(?:\.\d+)*)(?:(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|\d+[a-z]*))*)(?:\+\S+)?$/im;
+            /^java\s+(?:[^\s-]+-)?(?<version>\d{1,2}(?:\.\d+){0,2})(?=[-+_.\s]|$)/im;
     }
     else {
         javaVersionRegExp = /(?<version>(?<=(^|\s|-))(\d+\S*))(\s|$)/;
