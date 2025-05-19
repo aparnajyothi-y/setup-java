bb442ad81e5948b24aa3d7f3be1290bca4748e7d
Aparna Jyothi <aparnajyothi-y@github.com>
2025-05-19 17:20:46 +0530

    regex update

--- src/util.ts
+++ src/util.ts
@@ -133,7 +133,7 @@ export function getVersionFromFileContent(
   const versionFileName = getFileName(versionFile);
   if (versionFileName == '.tool-versions') {
     javaVersionRegExp =
-       /^java\s+(?:[^\s-]+-)?(?<version>\d+(?:\.\d+){1,2}(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|u\d+|\d+[a-z]*))?(?:[+_](?:\d+(?:\.\d+)?(?:\.LTS)?))?)/im;
+      /^java\s+(?:\S*-)?(?<version>\d+(?:\.\d+)*)(?:(?:[-_.](?:ea|LTS|beta|snapshot|internal|b\d+|\d+[a-z]*))*)(?:\+\S+)?$/im;
   } else {
     javaVersionRegExp = /(?<version>(?<=(^|\s|-))(\d+\S*))(\s|$)/;
   }
